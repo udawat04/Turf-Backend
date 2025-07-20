a83777eb02ad65d17ff2d02df02b4bee93e2e108
sudarshan udawat <itzudawat@gmail.com>
2025-07-20 23:27:28 +0530

    Remove otp functionality at time of login

--- Controller/userController.js
+++ Controller/userController.js
@@ -5,93 +5,105 @@ const jwt = require("jsonwebtoken");
 const secretKey = "kajdakdoermjsfsfskfjswoeidm";
 
 exports.createUser = async (req,res)=>{
-    const { name, email, phone, password, otp, validTime,role } = req.body;
-    
-    //checking is data is not empty
-    if(!(name && email && phone && password)){
-        return res.status(400).json({ msg: "all fields are required" }); 
-    }
-    
-    // find that email is already exist or not
-    const alreadyEmail = await User.findOne({email})
+  try {
+      const { name, email, phone, password, otp, validTime, role } = req.body;
 
-    if(alreadyEmail){
-        return res.status(400).json({msg:"email is already exist"})
-    }
+      //checking is data is not empty
+      if (!(name && email && phone && password)) {
+        return res.status(400).json({ msg: "all fields are required" });
+      }
 
-    // otp genreate function is created 
-    const genOtp = (length)=>{
-        const otp = Math.floor(Math.random(length)*Math.pow(10,length))
-        return otp
-    }
+      // find that email is already exist or not
+      const alreadyEmail = await User.findOne({ email });
 
-    const newOtp = genOtp(4) // function is called
-    console.log(newOtp);
-    
-    // otp time is created
-    const otpTime = moment().format()
+      if (alreadyEmail) {
+        return res.status(400).json({ msg: "email is already exist" });
+      }
 
-    // create hash password 
-    const salt = bcrypt.genSaltSync(10)
-    const hash = bcrypt.hashSync(password,salt)
-    
+      // otp genreate function is created
+      const genOtp = (length) => {
+        const otp = Math.floor(Math.random(length) * Math.pow(10, length));
+        return otp;
+      };
 
-    const data = {name,email,phone,role,password:hash,otp:newOtp,validTime:otpTime}
+      const newOtp = genOtp(4); // function is called
+      console.log(newOtp);
 
-    const newUser = new User(data)
-    await newUser.save()
+      // otp time is created
+      const otpTime = moment().format();
 
-    return res.status(200).json({msg:"user created successfully",newUser})
+      // create hash password
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(password, salt);
+
+      const data = {
+        name,
+        email,
+        phone,
+        role,
+        password: hash,
+        otp: newOtp,
+        validTime: otpTime,
+      };
+
+      const newUser = new User(data);
+      await newUser.save();
+
+      return res
+        .status(200)
+        .json({ msg: "user created successfully", newUser });
+  } catch (error) {
+      return res.status(400).json({ error: error.message });
+  }
 }
 
 exports.getUser = async(req,res) => {
-    const result = await User.find()
-    return res.status(200).json({msg:"user get successfully",result})
+   try {
+     const result = await User.find();
+     return res.status(200).json({ msg: "user get successfully", result });
+   } catch (error) {
+    return res.status(400).json({ error: error.message });
+   }
 }
 
 exports.login = async (req,res)=>{
-const {email,password,otp} =req.body
+try {
+    const { email, password} = req.body;
 
-const alreadyEmail = await User.findOne({email})
+    const alreadyEmail = await User.findOne({ email });
 
-if(!alreadyEmail){
-    return res.status(400).json({msg:"user is not created , first signup "})
-}
-
-const dbpassword = alreadyEmail.password
-const dbotp = alreadyEmail.otp
-
-const otpTime = alreadyEmail.validTime
-
-const data = alreadyEmail
-
-const match = await bcrypt.compare(password,dbpassword)
-
-if(!match){
-    return res.status(400).json({msg:"password is not match"})
-}
-
-const token = jwt.sign({ email: alreadyEmail.email }, secretKey);
-console.log(token);
-
-if(otp===dbotp){
-    const current = moment()
-    const timeValid = current.diff(otpTime,"minutes")
-    console.log("otp valid time",timeValid);
-
-    if(timeValid>10){
-        return res.status(400).json({msg:"Otp is expire , please genrate new otp"})
+    if (!alreadyEmail) {
+      return res
+        .status(400)
+        .json({ msg: "user is not created , first signup " });
     }
+
+    const dbpassword = alreadyEmail.password;
+   
+
+    const data = alreadyEmail;
+
+    const match = await bcrypt.compare(password, dbpassword);
+
+    if (!match) {
+      return res.status(400).json({ msg: "password is not match" });
+    }
+
+    const token = jwt.sign({ email: alreadyEmail.email }, secretKey);
+    console.log(token);
 
     return res
       .status(200)
-      .json({ msg: "otp is matched,User login successfully" ,data,token});
+      .json({ msg: "User login successfully", data, token });
+} catch (error) {
+   console.error("Error in createUser:", error); // log it for debugging
+   return res
+     .status(500)
+     .json({ error: error?.message || "Internal Server Error" });
 }
-else{
-    return res
-      .status(400)
-      .json({ msg: "Otp is invalid or not matched" });
-}
+
+
+
 
 
 }
